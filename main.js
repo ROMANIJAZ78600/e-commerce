095c7c7c517d1217563519e73b87a9c171455304
Roman ijaz <ejazahmed4720@gmail.com>
2025-04-29 18:54:53 +0500

    Shoping

---
+++ main.js
@@ -0,0 +1,35 @@
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl)
+})
+
+
+
+function showToast(){
+    toastList[0].show()
+}
+
+function gotodetails(){
+    window.location.href =  'pdetails.html';
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
